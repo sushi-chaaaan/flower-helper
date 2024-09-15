debdd178713e8ed3aa702ecfcff82d7cdf0a05a7
sushi-chaaaan <mail@sushichan.live>
2024-09-15 16:37:35 +0900

    いい感じセットアップ

--- apps/discord/src/index.ts
+++ apps/discord/src/index.ts
@@ -1,9 +1,26 @@
-import { Hono } from "hono";
+import { type APIInteraction, InteractionResponseType, InteractionType } from "discord-api-types/v10";
+import { honoFactory } from "./lib/hono";
+import { verifyDiscordInteraction } from "./middleware/discord";
 
-const app = new Hono();
+const app = honoFactory.createApp();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Hello, World!");
+});
+
+app.get("/hello-world", (c) => {
+  const requestId = c.get("requestId");
+  return c.text(`Hello, World! Request ID: ${requestId}`);
+});
+
+app.post("/interactions", verifyDiscordInteraction, async (c) => {
+  const interaction = await c.req.json<APIInteraction>();
+
+  if (interaction.type === InteractionType.Ping) {
+    return c.json({ type: InteractionResponseType.Pong });
+  }
+
+  return c.json({ type: InteractionResponseType.Pong });
 });
 
 // ここから下は fallback 的な Routing
--- apps/discord/src/lib/hono.ts
+++ apps/discord/src/lib/hono.ts
@@ -1,17 +1,26 @@
 import { csrf } from "hono/csrf";
 import { createFactory } from "hono/factory";
 import { poweredBy } from "hono/powered-by";
-import { requestId } from "hono/request-id";
-import { secureHeaders } from "hono/secure-headers";
+import { type RequestIdVariables, requestId } from "hono/request-id";
+import { NONCE, type SecureHeadersVariables, secureHeaders } from "hono/secure-headers";
 import { trimTrailingSlash } from "hono/trailing-slash";
 
 type HonoConfig = {
   Bindings: Env;
+  Variables: {} & RequestIdVariables & SecureHeadersVariables;
 };
 
 const honoFactory = createFactory<HonoConfig>({
   initApp: (app) => {
-    app.use(csrf(), secureHeaders(), trimTrailingSlash(), requestId(), poweredBy());
+    app.use(csrf(), trimTrailingSlash(), requestId(), poweredBy());
+    app.get(
+      "*",
+      secureHeaders({
+        contentSecurityPolicy: {
+          scriptSrc: [NONCE]
+        }
+      })
+    );
   }
 });
 
